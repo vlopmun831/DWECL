fc524d1105564794bbec6e80df0155793424ffbf
vlopmun831 <vanesaopos81@gmail.com>
2025-09-17 12:50:16 +0200

    resulelto

--- UNIDAD1/script.js
+++ UNIDAD1/script.js
@@ -3,6 +3,16 @@
 
             }
 
+        function pintar_ruta_v2(){
+            var substrng1 = "El acceso a la ruta C:\\usuario\\tarda 1'23\,algo" ;
+
+            var substrng2 = "considerado \ 'lento' \ en la actualidad.";
+                console.log(substrng1 + substrng2);
+                    }
+
+
+
+
         function saludar(){
             var edad = window.prompt("Introduce tu edad");
             if (edad >= 18){
@@ -12,5 +22,21 @@
                 console.log("Eres menor de edad");
             }
         }
+
+
+        //Ejercicio 4
+        function mostrar_potencia() {
+                var num =2*Math.pow(10,-9);
+                   console.log("El valor de 2*10^-9 es : " + num);
+        }
    
+
+        //Ejercicio 5
+        function mostrar_n_distintas_bases(num){
+            //transforma a binario
+            var numDecimal = num.toString(2);
+
+                console.log (numDecimal);
+        }
+       
     

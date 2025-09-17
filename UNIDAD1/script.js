02bb8960e2099d868c30c7ff4046f0c9677a8b64
vlopmun831 <vanesaopos81@gmail.com>
2025-09-17 12:16:24 +0200

    primer comit

---
+++ UNIDAD1/script.js
@@ -0,0 +1,16 @@
+        function pintar_ruta(){
+            console.log("El acceso a la ruta C:\\usuario\\tarda 1'23\",algo considerado\ 'lento' en la actualidad." );
+
+            }
+
+        function saludar(){
+            var edad = window.prompt("Introduce tu edad");
+            if (edad >= 18){
+                console.log("Eres mayor de edad");
+
+            }else{
+                console.log("Eres menor de edad");
+            }
+        }
+   
+    

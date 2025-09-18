be2e471f091be6a034fc7f94a46b361b94a70118
vlopmun831 <vanesaopos81@gmail.com>
2025-09-18 13:48:07 +0200

    subida

--- UNIDAD1/script.js
+++ UNIDAD1/script.js
@@ -1,8 +1,11 @@
+        //Ejercicio 2
+        
         function pintar_ruta(){
             console.log("El acceso a la ruta C:\\usuario\\tarda 1'23\",algo considerado\ 'lento' en la actualidad." );
 
             }
 
+        //Ejercicio 3
         function pintar_ruta_v2(){
             var substrng1 = "El acceso a la ruta C:\\usuario\\tarda 1'23\,algo" ;
 
@@ -12,7 +15,7 @@
 
 
 
-
+        //Ejercicio 1
         function saludar(){
             var edad = window.prompt("Introduce tu edad");
             if (edad >= 18){
@@ -39,4 +42,54 @@
                 console.log (numDecimal);
         }
        
-    
+        //Ejercicio 6
+        function division_operar(num){
+            var resultado = num /0;
+            var total = resultado + 23;
+            console.log("Resultado de dividir " + total)
+        }
+
+        //Ejercicio 7
+
+
+
+        //Ejercicio 10
+        function check_ambito_variable(){
+            let prueba = 5;
+            console.log("la variable de prueba tiene como valor " + prueba);
+
+        {
+            let prueba2 = 10;
+             console.log("Insisto, la variable de prueba tiene el valor" +  prueba + "también dentro del bloque anidado" );
+             console.log("Además, el bloque anterior alberga la variable prueba2  que tiene el valor "+  prueba2);
+
+        }
+
+        console.log("Por último, desde el el bloque principal, intento acceder al valor de una variable interna: "+prueba);
+            
+    }
+
+
+        //Ejercicio 11
+        function definir_array(){
+            let array = ["Alberto", "Juan" , "Sofía", "Pepe"];
+
+           /* for(let i = 0; i < array.length; i ++){
+                console.log(array[i]);
+            }*/
+
+                console.table(array);
+        }
+
+        //Ejercicio 12
+        function contar_y_temporizar(){
+            let valor = 0;
+
+            let iniTime = new Date();
+            for( let i =0; i<100000;i++){
+                valor = valor +10;
+            }
+            let endTime = new Date();
+            console.log("Valor : " + valor);
+            console.log("El algoritmo ha tardado " + (endTime - iniTime) + " segundos");
+        }

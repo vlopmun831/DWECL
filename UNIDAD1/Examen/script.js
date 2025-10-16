319accb45a0629c4a6cd058542cd41ccbdbfd3cc
vlopmun831 <vanesaopos81@gmail.com>
2025-10-16 09:20:41 +0200

    Subida

---
+++ UNIDAD1/Examen/script.js
@@ -0,0 +1,15 @@
+//Ejercicio 1
+
+function analiza_frase(){
+    //inicializamos variables
+    var numchar =0;
+    var frase= prompt("Introduce una frase");
+    console.log(frase);
+    //TEner o no tener en cuenta los 
+    
+    console.log()
+
+    for(let i =i; i < frase.length;i++){
+
+    }
+}

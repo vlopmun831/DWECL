a0fe9e098179ffa7d6bd8b611d83da118b249245
vlopmun831 <vanesaopos81@gmail.com>
2025-11-12 12:37:09 +0100

    sub

--- UNIDAD2/boletin2/Resultado_prueba/script.js
+++ UNIDAD2/boletin2/Resultado_prueba/script.js
@@ -1,40 +1,85 @@
+//  window.onload = function() {
+//     cargar_datos_cookie();
+//   };
 function ejecutar_accion() {
-    var resultado = document.getElementById("resultado");
     var select = document.getElementById('opciones');
     var opcion = select.value;
     switch (opcion) {
         case ("1"):
-            nombreAlReves();
+            nombre_reves();
             break;
         case ("2"):
             redirigir();
             break;
         case ("3"):
-            calculaEdad();
+            calcula_edad();
             break;
         case ("4"):
-            coockies();
+            almacena_coockies();
+            break;
+        default:
+            console.log("Opción no válida");
             break;
     }
-    console.log("Opción no válida");
-}
-function nombreAlReves() {
-    var _a;
-    var resultado = document.getElementById("resultado");
-    var input = document.getElementById("nombre");
-    var nombre = input.value;
-    var input2 = document.getElementById("fechaNacimiento");
-    var fechaNacimiento = input2.valueAsDate;
-    var nombreReves = "";
-    for (var i = nombre.length - 1; i >= 0; i--) {
-        nombreReves += nombre[i];
-        resultado.textContent = nombreReves.toUpperCase() + ((_a = input2.valueAsDate) === null || _a === void 0 ? void 0 : _a.getFullYear());
+    function nombre_reves() {
+        var _a;
+        var resultado = document.getElementById("resultado");
+        var input = document.getElementById("nombre");
+        var nombre = input.value;
+        var input2 = document.getElementById("fechaNacimiento");
+        var fechaNacimiento = input2.valueAsDate;
+        var nombreReves = "";
+        for (var i = nombre.length - 1; i >= 0; i--) {
+            nombreReves += nombre[i];
+            resultado.textContent = nombreReves.toUpperCase() + ((_a = input2.valueAsDate) === null || _a === void 0 ? void 0 : _a.getFullYear());
+        }
+        return nombreReves;
     }
-    return nombreReves;
-}
-function mensaje_error() {
-    var resultado = document.getElementById("resultado");
+    function mensaje_error() {
+        var resultado = document.getElementById("resultado");
+        resultado.textContent = " ";
+        var errorparrafo = document.createElement("p");
+        errorparrafo.style.color = "red";
+        errorparrafo.style.fontWeight = "bold";
+        errorparrafo.textContent = "Debe incluir la cabecera https:// en la URL";
+        resultado.appendChild(errorparrafo);
+    }
     function redirigir() {
-        let;
+        var input2 = document.getElementById("apellido");
+        var input = document.getElementById("url");
+        var url = input.value;
+        var regexp = new RegExp("^https:\/\/");
+        if (regexp.test(url)) {
+            var apellido = input2.value.split(" ")[0];
+            var direccion = url + "/search?q=" + apellido;
+            window.location.href = direccion;
+        }
+        else {
+            mensaje_error();
+        }
+    }
+    function calcula_edad() {
+        var input = document.getElementById("fechaNacimiento");
+        var fechaNacimiento = input.valueAsDate;
+        var resultado = document.getElementById("resultado");
+        var fechaActual = new Date();
+        var anioActual = fechaActual.getFullYear();
+        var edad = anioActual - fechaNacimiento.getFullYear();
+        resultado.textContent = "Tienes " + edad + " años";
+    }
+    function almacena_coockies() {
+        var input = document.getElementById("nombre");
+        var input2 = document.getElementById("apellido");
+        var input3 = document.getElementById("edad");
+        var input4 = document.getElementById("telefono");
+        var input5 = document.getElementById("fechaNacimiento");
+        var input6 = document.getElementById("url");
+        document.cookie = "nombre=" + input.value;
+        document.cookie = "apellido=" + input.value;
+        document.cookie = "edad=" + input3.value;
+        document.cookie = "telefono=" + input4.value;
+        document.cookie = "fechaNacimiento=" + input5.value;
+        document.cookie = "url=" + input6.value;
+        console.table(document.cookie);
     }
 }
--- UNIDAD2/boletin2/Resultado_prueba/script.ts
+++ UNIDAD2/boletin2/Resultado_prueba/script.ts
@@ -1,10 +1,13 @@
+//  window.onload = function() {
+//     cargar_datos_cookie();
+//   };
+ 
  function ejecutar_accion():void{
-    let resultado = document.getElementById("resultado") as HTMLDivElement;
     let select: HTMLSelectElement = document.getElementById('opciones') as HTMLSelectElement;
     let opcion: string = select.value;
     switch(opcion){
         case ("1"):
-            nombreAlReves();
+            nombre_reves();
             break;
         case ("2"):
             redirigir();
@@ -12,27 +15,27 @@
             break;
 
         case ("3"):
-            calculaEdad();
+            calcula_edad();
             break;
 
         case ("4"):
-            coockies();
+            almacena_coockies();
             break;
     
-
-    }
     default:
         console.log("Opción no válida");
+        break;
+
 }
 
 
 
-function nombreAlReves(): string {
+function nombre_reves(): string {
     let resultado = document.getElementById("resultado") as HTMLDivElement;
     let input: HTMLInputElement = document.getElementById("nombre") as HTMLInputElement;
     let nombre: string = input.value;
     let input2: HTMLInputElement = document.getElementById("fechaNacimiento") as HTMLInputElement;
-    let fechaNacimiento: string = input2.valueAsDate;
+    let fechaNacimiento: Date = input2.valueAsDate as Date;
     let nombreReves: string = "";       
     for(let i = nombre.length - 1; i >= 0; i--){
         nombreReves += nombre[i];
@@ -44,8 +47,59 @@ function nombreAlReves(): string {
 }
 
 function mensaje_error(): void {
-    let resultado = document.getElementById("resultado")
+    let resultado :HTMLDivElement= document.getElementById("resultado") as HTMLDivElement
+    resultado.textContent = " ";
+    let errorparrafo = document.createElement("p");
+    errorparrafo.style.color = "red";
+    errorparrafo.style.fontWeight = "bold";
+    errorparrafo.textContent = "Debe incluir la cabecera https:// en la URL";
+    resultado.appendChild(errorparrafo);  
+}
 
 
-function redirigir(): void {
-    let
+function redirigir(): void {    
+    let input2:HTMLInputElement = document.getElementById("apellido") as HTMLInputElement;
+    let input:HTMLInputElement = document.getElementById("url") as HTMLInputElement;
+    let url: string = input.value;
+    let regexp = new RegExp("^https:\/\/");
+    if(regexp.test(url)){
+       let apellido :string = input2.value.split(" ")[0];
+       let direccion:string = url + "/search?q=" + apellido;
+        window.location.href = direccion;
+    }else{
+        mensaje_error();
+
+    }
+}
+
+
+function calcula_edad(): void {
+    let input:HTMLInputElement = document.getElementById("fechaNacimiento") as HTMLInputElement;
+    let fechaNacimiento: Date = input.valueAsDate as Date;
+    let resultado: HTMLDivElement = document.getElementById("resultado") as HTMLDivElement;
+    let fechaActual = new Date();
+    let anioActual = fechaActual.getFullYear();
+    let edad = anioActual - fechaNacimiento.getFullYear();
+    resultado.textContent = "Tienes " + edad + " años";
+
+}
+
+
+function almacena_coockies(): void {
+    let input:HTMLInputElement = document.getElementById("nombre") as HTMLInputElement;
+    let input2:HTMLInputElement = document.getElementById("apellido") as HTMLInputElement; 
+    let input3:HTMLInputElement = document.getElementById("edad") as HTMLInputElement;
+    let input4:HTMLInputElement = document.getElementById("telefono") as HTMLInputElement;
+    let input5:HTMLInputElement = document.getElementById("fechaNacimiento") as HTMLInputElement;
+    let input6:HTMLInputElement = document.getElementById("url") as HTMLInputElement;
+
+    document.cookie = "nombre=" + input.value;
+    document.cookie = "apellido=" + input.value;
+    document.cookie = "edad=" + input3.value;
+    document.cookie = "telefono=" + input4.value;
+    document.cookie = "fechaNacimiento=" + input5.value;
+    document.cookie = "url=" + input6.value;
+    console.table(document.cookie);
+
+ }
+}
